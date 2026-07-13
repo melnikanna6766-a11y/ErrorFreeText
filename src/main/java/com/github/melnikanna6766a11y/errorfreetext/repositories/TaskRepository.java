da609de8fa6c6859fe011c7a838ce690b66f43b7
Anna <melana0607061@gmail.com>
2026-07-13 21:22:15 +0300

    criteria query

--- src/main/java/com/github/melnikanna6766a11y/errorfreetext/repositories/TaskRepository.java
+++ src/main/java/com/github/melnikanna6766a11y/errorfreetext/repositories/TaskRepository.java
@@ -1,13 +1,7 @@
 package com.github.melnikanna6766a11y.errorfreetext.repositories;
 
 import com.github.melnikanna6766a11y.errorfreetext.entity.Task;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
-    @EntityGraph()
-    @Query("select t from Tasks t where ")
-    public Task findEarliestCreatedTask();
 }
--- src/main/java/com/github/melnikanna6766a11y/errorfreetext/services/TaskScheduler.java
+++ src/main/java/com/github/melnikanna6766a11y/errorfreetext/services/TaskScheduler.java
@@ -1,16 +1,16 @@
 package com.github.melnikanna6766a11y.errorfreetext.services;
 
 import com.github.melnikanna6766a11y.errorfreetext.entity.Task;
-import com.github.melnikanna6766a11y.errorfreetext.repositories.TaskRepository;
+import com.github.melnikanna6766a11y.errorfreetext.repositories.TaskCriteriaRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskScheduler {
-    private TaskRepository taskRepository;
+    private TaskCriteriaRepository taskCriteriaRepository;
 
     @Scheduled(fixedRate = 2000)
     public void handleTask() {
-
+        Task task = taskCriteriaRepository.findEarliestCreatedTask();
     }
 }
